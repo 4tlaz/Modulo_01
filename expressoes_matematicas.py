ff1b1da441ec32b29fcad621fc54ede0c7581f50
Eduardo Muniz <eduardomuniz1182@gmail.com>
2023-11-07 19:35:03 -0300

    Enviando primeira atividade

---
+++ expressoes_matematicas.py
@@ -0,0 +1,17 @@
+# --> Atividade da Semana 01 <--
+
+# Primeira Expressão: 10 + 20 x 30
+resultado = 10 + 20 * 30
+print(f"O resultado da Primeira Expressão é: {resultado}")
+
+# Segunda Expressão: 42 ÷ 30
+resultado = 42 / 30
+print(f"O resultado da Segunda Expressão é: {resultado}")
+
+# Terceira Expressão: (9^4 + 2) x 6 – 1
+resultado = (9 ** 4 + 2) * 6 - 1
+print(f"O resultado da Terceira Expressão é: {resultado}")
+
+# Quarta Expressão: 10 % 3 * 10 ** 2 + 1 – 10 * 4 / 2
+resultado = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+print(f"O resultado da Quarta Expressão é: {resultado}")
